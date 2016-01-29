34ccc029a2b677e0eee0eb0df4525dff09c00e81
Giuseppe Nucifora <me@giuseppenucifora.com>
2016-01-29 12:31:02 +0100

    - Release 0.1.0

---
+++ Example/Pods/Target Support Files/Pods-ToucheableScrollView_Tests/Pods-ToucheableScrollView_Tests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_ToucheableScrollView_TestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_ToucheableScrollView_TestsVersionString[];
+
---
+++ Example/Pods/Target Support Files/ToucheableScrollView/ToucheableScrollView-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ToucheableScrollView : NSObject
+@end
+@implementation PodsDummy_ToucheableScrollView
+@end
